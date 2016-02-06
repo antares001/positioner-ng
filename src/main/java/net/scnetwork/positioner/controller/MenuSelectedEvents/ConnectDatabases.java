15b17f413dc564764693ea0c9e54d0061411ae07
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-02-06 10:14:47 +0200

    make form database connection

--- src/main/java/net/scnetwork/positioner/controller/MenuSelectedEvents/ConnectDatabases.java
+++ src/main/java/net/scnetwork/positioner/controller/MenuSelectedEvents/ConnectDatabases.java
@@ -2,9 +2,7 @@ package net.scnetwork.positioner.controller.MenuSelectedEvents;
 
 import com.vaadin.server.Sizeable;
 import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.MenuBar;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.Window;
+import com.vaadin.ui.*;
 import net.scnetwork.positioner.controller.MainView;
 import net.scnetwork.positioner.utils.HiddenVariable;
 
@@ -23,6 +21,23 @@ public class ConnectDatabases implements MenuBar.Command{
         window.setModal(true);
         window.setHeight(300.0f, Sizeable.Unit.PIXELS);
         window.setWidth(800.0f, Sizeable.Unit.PIXELS);
+
+        CustomLayout connectLayout = new CustomLayout("databaseConnect");
+        ComboBox typeConnection = new ComboBox();
+        typeConnection.addItems("PostgreSQL");
+        typeConnection.addItems("MySQL");
+        connectLayout.addComponent(typeConnection, "typeConnect");
+
+        TextField username = new TextField();
+        connectLayout.addComponent(username, "loginConnect");
+
+        PasswordField password = new PasswordField();
+        connectLayout.addComponent(password, "passConnect");
+
+        TextField database = new TextField();
+        connectLayout.addComponent(database, "nameDatabaseConnect");
+
+        window.setContent(connectLayout);
 
         UI.getCurrent().addWindow(window);
     }
