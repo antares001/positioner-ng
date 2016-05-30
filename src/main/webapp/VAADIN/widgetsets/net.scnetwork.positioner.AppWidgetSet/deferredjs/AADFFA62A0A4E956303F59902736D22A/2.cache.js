87d45171cf7ee70d81966d999ae90cafb8f0968f
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-05-30 23:30:07 +0300

    add hibernate

---
+++ src/main/webapp/VAADIN/widgetsets/net.scnetwork.positioner.AppWidgetSet/deferredjs/AADFFA62A0A4E956303F59902736D22A/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.net_scnetwork_positioner_AppWidgetSet.runAsyncCallback2('peb(1589,1,n3d);_.Nc=function hjc(){v4b((!o4b&&(o4b=new A4b),o4b),this.a.d)};uYd(mj)(2);\n//# sourceURL=net.scnetwork.positioner.AppWidgetSet-2.js\n')
