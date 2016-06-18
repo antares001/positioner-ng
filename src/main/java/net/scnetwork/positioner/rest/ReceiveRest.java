6fb148a4c6a195713809a5fba0d53db62c0bc21a
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-06-18 14:52:04 +0300

    sss

--- src/main/java/net/scnetwork/positioner/rest/ReceiveRest.java
+++ src/main/java/net/scnetwork/positioner/rest/ReceiveRest.java
@@ -1,13 +1,26 @@
 package net.scnetwork.positioner.rest;
 
+import com.topografix.gpx._1._1.GpxType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 public class ReceiveRest {
     @RequestMapping("/rest/put/test")
     public String EchoTest(@RequestParam(value = "name", defaultValue = "test") String name){
         return name;
+    }
+
+    @RequestMapping("/rest/put/data/{id}/{name}")
+    public String ReceiveGetData(@PathVariable(value = "id") String id,
+                                 @RequestParam(value = "lat") BigDecimal lat,
+                                 @RequestParam(value = "lon") BigDecimal lon,
+                                 @RequestParam(value = "alt") BigDecimal alt,
+                                 @RequestParam(value = "name", defaultValue = "origin") String name){
+        return "success";
     }
 }
