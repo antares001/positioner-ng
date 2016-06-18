19064b96386fdd61c89740597dd3413eacd07dbf
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-06-18 14:59:12 +0300

    sss

--- src/main/java/net/scnetwork/positioner/rest/GpxRest.java
+++ src/main/java/net/scnetwork/positioner/rest/GpxRest.java
@@ -8,36 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GpxRest {
     @RequestMapping(value = "/rest/gpx/test", method = RequestMethod.GET/*, produces = MediaType.APPLICATION_XML_VALUE*/)
-    public ObjectFactory SampleGpx(){
-        ObjectFactory gpx = new ObjectFactory();
+    public GpxType SampleGpx(){
+        ObjectFactory factory = new ObjectFactory();
+        GpxType gpx = factory.createGpxType();
 
-        GpxType gpxType = new GpxType();
-        gpxType.setCreator("positioner-ng");
-        gpxType.setVersion("0.1");
-
-        gpxType.setExtensions(new ExtensionsType());
-
-        MetadataType metadataType = new MetadataType();
-        metadataType.setName("some_name");
-        metadataType.setDesc("positioner system");
-        metadataType.setKeywords("keywords");
-
-        PersonType personType = new PersonType();
-        personType.setName("Nikolai Bloshkin");
-
-        EmailType emailType = new EmailType();
-        emailType.setId("bloshkin.nikolai");
-        emailType.setDomain("gmail.com");
-        personType.setEmail(emailType);
-
-        LinkType linkType = new LinkType();
-        linkType.setHref("github.com/antares001/positioner-ng");
-        linkType.setText("positioner-ng");
-        linkType.setType("https");
-        personType.setLink(linkType);
-        metadataType.setAuthor(personType);
-        gpxType.setMetadata(metadataType);
-        gpx.createGpx(gpxType);
+        gpx.setCreator("sun");
+        gpx.setMetadata(factory.createMetadataType());
+        gpx.setExtensions(factory.createExtensionsType());
+        gpx.setVersion("0.1");
 
         return gpx;
     }
