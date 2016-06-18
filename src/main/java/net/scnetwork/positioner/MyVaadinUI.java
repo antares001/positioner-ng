eb117aaf179913ee2363512e5626e41a224fe181
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-06-18 18:59:40 +0300

    valo

--- src/main/java/net/scnetwork/positioner/MyVaadinUI.java
+++ src/main/java/net/scnetwork/positioner/MyVaadinUI.java
@@ -11,7 +11,7 @@ import net.scnetwork.positioner.controller.MainView;
 import net.scnetwork.positioner.controller.Settings;
 
 
-@Theme("MyTheme")
+@Theme("valo")
 @SpringUI
 public class MyVaadinUI extends UI {
     @Override
