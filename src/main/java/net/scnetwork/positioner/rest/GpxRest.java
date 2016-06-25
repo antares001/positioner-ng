4499c28e8c97066b321b8fca042cbd9dff2d801e
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-06-25 14:46:43 +0300

    add new methods

--- src/main/java/net/scnetwork/positioner/rest/GpxRest.java
+++ src/main/java/net/scnetwork/positioner/rest/GpxRest.java
@@ -1,16 +1,19 @@
 package net.scnetwork.positioner.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.topografix.gpx._1._1.GpxType;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/rest/gpx")
 public class GpxRest {
     @RequestMapping(value = "/echo", method = RequestMethod.GET)
-    public String  SampleGpx(@RequestParam(value = "name", defaultValue = "-") String name) {
+    public String  getTest(@RequestParam(value = "name", defaultValue = "-") String name) {
         return name;
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public GpxType getGpx(@PathVariable(value = "id") String id){
+        return null;
     }
 }
